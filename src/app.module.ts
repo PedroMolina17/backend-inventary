69ce5e6fb4ab32be2faff61a445a1e80e93c00a3
pedromolina17 <pedro.molinanoa@gmail.com>
2024-10-12 21:43:13 -0500

    Add JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,18 @@ import { CategoryModule } from './category/category.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'images'),
+    }),
+    PassportModule,
+    JwtModule.register({
+      secret: 'mySecretKey',
+      signOptions: { expiresIn: '60m' },
     }),
     PrismaModule,
     ProductModule,
